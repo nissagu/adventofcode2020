56b2bf8a8e8e463d21abd1201f8d3a700b4a6a1f
Ruth Papacek <ruthp@posteo.de>
2020-12-13 22:00:29 +0100

    added method for part 2 (comments partially missing)

--- Day_7/HandyHaversacks/Program.cs
+++ Day_7/HandyHaversacks/Program.cs
@@ -13,7 +13,7 @@ namespace HandyHaversacks
             List<String> allEntries = new List<string>();
             List<Bag> allBags;
 
-            using (StreamReader streamReader = new StreamReader("/Users/Ruth/Projects/Advent_of_Code/Day_7/test.txt"))
+            using (StreamReader streamReader = new StreamReader("/Users/Ruth/Projects/Advent_of_Code/Day_7/bagrules.txt"))
             {
 
                 while (streamReader.Peek() >= 0)
@@ -27,18 +27,72 @@ namespace HandyHaversacks
             allBags = BagParser.GenerateData(allEntries);
             Console.WriteLine(allBags.Count());
 
+            // part 1
             var color = "shiny gold";
             Console.WriteLine(GetAllOuterBagsForColor(allBags, color));
 
-            Console.WriteLine(GetSumOfContainingBags(allBags, color));
+            // part 2
+            var shinyGoldBag = allBags.Find(bag => bag.OuterColor.Equals("shiny gold"));
+
+            var amount = GetAmountOfSpecificBag(allBags, shinyGoldBag);
+            Console.WriteLine(amount);
 
         }
 
-        private static int GetSumOfContainingBags(List<Bag> allBags, string color)
+        // returns amount of all the bags inside a specific bag (including the nested inner bags)
+        private static int GetAmountOfSpecificBag(List<Bag> allBags, Bag bag)
         {
-            var sum = 0;
+            var amount = 0;
 
-            return sum;
+            // get a list of all inner bags for the committed bag
+            var allInnerBags = GetAllInnerBagsForSpecificBag(allBags, bag);
+
+            // if the bag doesn't have any bags inside, just retourn 0 as amount
+            if (allInnerBags.Any())
+            {
+                // go through each of the inner bags
+                foreach (var innerBag in allInnerBags)
+                {
+                    // find out how many bags of the inner bag are inside the committed bag
+                    // inner bag's outer color = key in bag's color dictionary
+                    var innerBagAmount = bag.ColorDictionary.FirstOrDefault(entry => entry.Key.Equals(innerBag.OuterColor)).Value;
+
+                    // 
+                    var innerAmountOfInnerBag = GetAmountOfSpecificBag(allBags, innerBag);
+
+                    //
+                    var innerBagProduct = innerBagAmount * innerAmountOfInnerBag;
+
+                    //
+                    amount += innerBagProduct + innerBagAmount;
+                }
+            }
+
+            return amount;
+        }
+
+        // return list of the bag objects that a specific bag contains
+        private static List<Bag> GetAllInnerBagsForSpecificBag(List<Bag> allBags, Bag bag)
+        {
+
+            var allColorsOfInnerBags = new List<string>();
+            var allInnerBags = new List<Bag>();
+
+            // first step: add color/s that initial bag contains to color list
+            foreach (var entry in bag.ColorDictionary)
+            {
+                allColorsOfInnerBags.Add(entry.Key);
+            }
+
+            // go through all the initial inner colors and find all bags that contain them
+            foreach (var color in allColorsOfInnerBags)
+            {
+                allInnerBags.Add(allBags.Find(bag => bag.OuterColor.Equals(color)));
+
+            }
+
+            return allInnerBags;
+
         }
 
         private static int GetAllOuterBagsForColor(List<Bag> allBags, string color)
@@ -53,7 +107,7 @@ namespace HandyHaversacks
             // go through all the searched for color containing bags and find all the bags that containing those bags
             for (int index = 0; index < allColorContainingBags.Count(); index++)
             {
-                var bagEntryColor = allColorContainingBags[index].Color;
+                var bagEntryColor = allColorContainingBags[index].OuterColor;
                 var bagsWithColor = GetColorContainingBags(allBags, bagEntryColor);
                 AddBagsToList(bagsWithColor, allColorContainingBags);
             }
@@ -65,7 +119,7 @@ namespace HandyHaversacks
         {
             foreach (var entry in inputList)
             {
-                var colorAlreadyInList = outputList.Find(bag => bag.Color.Equals(entry.Color)) is not null;
+                var colorAlreadyInList = outputList.Find(bag => bag.OuterColor.Equals(entry.OuterColor)) is not null;
 
                 if (!colorAlreadyInList)
                 {
@@ -78,7 +132,7 @@ namespace HandyHaversacks
 
         private static List<Bag> GetColorContainingBags(List<Bag> allBags, string color)
         {
-            return allBags.FindAll(entry => entry.ColorDictionary.Contains(color));
+            return allBags.FindAll(entry => entry.ColorDictionary.ContainsKey(color));
 
         }
     }
